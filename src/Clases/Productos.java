fc2dc0c2100d874c24cc946ea0ff0499ef4195b0
RogerAAG <rogeraag95@gmail.com>
2025-05-17 17:23:41 -0500

    Creacion clase Productos

---
+++ src/Clases/Productos.java
@@ -0,0 +1,43 @@
+package Clases;
+
+public class Productos {
+    private int idproducto;
+    private String descripcion;
+    private String categoria;
+    private int existencias;
+
+    public int getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(int idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getExistencias() {
+        return existencias;
+    }
+
+    public void setExistencias(int existencias) {
+        this.existencias = existencias;
+    }
+    
+    //Variables
+    //private final 
+}
